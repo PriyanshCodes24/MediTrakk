abd2906f7a9e106b8775ebc36acdf5c855dd72ad
Priyansh <prinshu.patel2004@gmail.com>
2025-06-19 09:51:24 +0530

    added change appointment status for doctor

--- server/controllers/appointmentController.js
+++ server/controllers/appointmentController.js
@@ -21,4 +21,44 @@ const createAppointment = async (req, res) => {
   }
 };
 
-module.exports = { createAppointment };
+const getDoctorAppointments = async (req, res) => {
+  try {
+    const doctorId = req.user.id;
+    const appointments = await Appointment.find({ doctor: doctorId }).populate(
+      "patient",
+      "name email"
+    );
+    return res.status(200).json({ appointments });
+  } catch (e) {
+    console.error(err.message);
+    return res.status(500).json({ message: "Server Error" });
+  }
+};
+
+const updateAppointmentStatus = async (req, res) => {
+  const doctor = req.user.id;
+  const { appointmentId } = req.params;
+  const { status } = req.body;
+
+  if (!["approved", "rejected"].includes(status))
+    return res.status(400).json({ message: "Invalid status value" });
+
+  const appointment = await Appointment.findOne({ _id: appointmentId, doctor });
+
+  if (!appointment)
+    return res.status(400).json({ message: "Appointment not found" });
+
+  appointment.status = status;
+  await appointment.save();
+
+  return res.status(200).json({
+    message: `Appointment status was updated to '${status}' successfully`,
+    appointment,
+  });
+};
+
+module.exports = {
+  createAppointment,
+  getDoctorAppointments,
+  updateAppointmentStatus,
+};
--- server/routes/appointmentRoute.js
+++ server/routes/appointmentRoute.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const { protect, authorizeRole } = require("../middleware/authMiddleware");
-const { createAppointment } = require("../controllers/appointmentController");
+const {
+  createAppointment,
+  getDoctorAppointments,
+  updateAppointmentStatus,
+} = require("../controllers/appointmentController");
 
 router.post("/", protect, authorizeRole("patient"), createAppointment);
+router.get("/doctor", protect, authorizeRole("doctor"), getDoctorAppointments);
+router.put(
+  "/:appointmentId/status",
+  protect,
+  authorizeRole("doctor"),
+  updateAppointmentStatus
+);
 
 module.exports = router;
