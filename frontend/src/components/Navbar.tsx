02ff092edb09aefad358a858da7027300c78da46
Priyansh <prinshu.patel2004@gmail.com>
2025-06-24 22:23:39 +0530

    added Logout

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,15 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext";
 
 export const Navbar = () => {
-  // const {}=use
+  const { user, logout } = useAuth();
+  const navigate = useNavigate();
+  const logoutHandler = () => {
+    if (window.confirm("Are you sure you want to sign out")) {
+      logout();
+      navigate("/login");
+    }
+  };
   return (
     <nav className="bg-blue-500 p-4 flex text-white justify-end">
       <div className="flex gap-4">
@@ -11,30 +19,36 @@ export const Navbar = () => {
         >
           Home
         </Link>
-        <Link
-          to="/login"
-          className="  hover:text-gray-200 hover:underline transition"
-        >
-          Sign In
-        </Link>
-        <Link
-          to="/register"
-          className="hover:text-gray-200 hover:underline transition"
-        >
-          Sign Up
-        </Link>
+        {!user && (
+          <Link
+            to="/login"
+            className="  hover:text-gray-200 hover:underline transition"
+          >
+            Sign In
+          </Link>
+        )}
+        {!user && (
+          <Link
+            to="/register"
+            className="hover:text-gray-200 hover:underline transition"
+          >
+            Sign Up
+          </Link>
+        )}
         <Link
           to="/dashboard"
           className=" hover:text-gray-200 hover:underline transition"
         >
           Dashboard
         </Link>
-        {/* <Link
-          to="/register"
-          className="hover:text-gray-200 hover:underline transition"
-        >
-          Sign Out
-        </Link> */}
+        {user && (
+          <button
+            className="hover:text-gray-200 hover:underline cursor-pointer transition"
+            onClick={logoutHandler}
+          >
+            Sign Out
+          </button>
+        )}
       </div>
     </nav>
   );
